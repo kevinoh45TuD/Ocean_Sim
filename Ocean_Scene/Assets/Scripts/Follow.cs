665508f0577bbdf9e21dd8930f65509af06d7262
KevinOh45 <61233166+kevinoh45TuD@users.noreply.github.com>
2021-03-08 02:54:29 +0000

    Movement setup

---
+++ Ocean_Scene/Assets/Scripts/Follow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    
+    public GameObject targetObj;
+
+    public Vector3 coords;
+    
+    void Update()
+    {
+        
+        
+        coords = new Vector3(targetObj.transform.position.x, targetObj.transform.position.y + 50, targetObj.transform.position.z);
+        gameObject.transform.position = coords;
+        transform.LookAt(targetObj.transform);
+    }
+    
+}
