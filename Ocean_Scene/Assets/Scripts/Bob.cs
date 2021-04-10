529dc09a7d2df794b1569aaab7a09946723e1de7
KevinOh45 <61233166+kevinoh45TuD@users.noreply.github.com>
2021-04-10 18:23:39 +0100

    Oocean/Boat

---
+++ Ocean_Scene/Assets/Scripts/Bob.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bob : MonoBehaviour
+{
+
+    public float heightChange;
+
+    public float changeTime;
+    public float currentTime;
+
+    public bool goingUp;
+
+    public Vector3 boatVector;
+    
+    void Update()
+    {
+        boatVector = new Vector3(0f, heightChange, 0f);
+        currentTime += Time.deltaTime;
+        
+        if (goingUp == true)
+        {
+            gameObject.transform.position += boatVector;
+        }
+        else
+        {
+            gameObject.transform.position -= boatVector;
+        }
+
+        if (currentTime >= changeTime)
+        {
+            goingUp = !goingUp;
+            currentTime = 0;
+        }
+    }
+        
+}
---
+++ Ocean_Scene/Assets/Scripts/Ocean_Motion.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ocean_Motion : MonoBehaviour
+{
+    public float scale;
+    public float waveSpeed;
+    public float waveHeight;
+    
+    void Update()
+    {
+        CalcNoise();
+    }
+    
+    void CalcNoise()
+    {
+            MeshFilter mF = GetComponent<MeshFilter>();
+    
+            Vector3[] verts = mF.mesh.vertices;
+    
+            for (int i = 0; i < verts.Length; i++)
+            {
+                float pX = (verts[i].x * scale) + (Time.time * waveSpeed);
+                float pZ = (verts[i].z * scale) + (Time.time * waveSpeed);
+    
+                verts[i].y = Mathf.PerlinNoise(pX, pZ) * waveHeight;
+            }
+    
+            mF.mesh.vertices = verts;
+    
+            mF.mesh.RecalculateNormals();
+    
+            mF.mesh.RecalculateBounds();
+    }
+    
+}
