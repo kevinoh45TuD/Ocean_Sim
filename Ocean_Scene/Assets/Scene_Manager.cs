3df0c8080d3dbe7a9518e13d30541d8c2beadadd
KevinOh45 <61233166+kevinoh45TuD@users.noreply.github.com>
2021-04-18 17:19:52 +0100

    more boats

---
+++ Ocean_Scene/Assets/Scene_Manager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene_Manager : MonoBehaviour
+{
+    public int cameraIndex;
+
+    public Camera[] sceneCameras;
+
+    public void nextCamera()
+    {
+        
+        sceneCameras[cameraIndex].gameObject.SetActive(false);
+        
+        if (cameraIndex == sceneCameras.Length -1)
+        {
+            cameraIndex = 0;
+        }
+        else
+        {
+            cameraIndex += 1;
+        }
+        
+        sceneCameras[cameraIndex].gameObject.SetActive(true);
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonUp("Jump"))
+        {
+            nextCamera();
+        }
+    }
+}
